c162647365fd36875544fd1ed6cb1b564b0161b5
Frédéric Massart <fred@branchup.tech>
2025-02-07 15:21:33 +0800

    Update file headers to the correct ones

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -1,30 +1,22 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Level Up Quest.
 //
-// Moodle is free software: you can redistribute it and/or modify
+// Level Up Quest is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// Level Up Quest is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Privacy provider.
- *
- * @package    availability_gearup
- * @copyright  2023 Frédéric Massart
- * @author     Frédéric Massart <fred@branchup.tech>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+// along with Level Up Quest.  If not, see <https://www.gnu.org/licenses/>.
+//
+// https://levelup.plus
 
 namespace availability_gearup\privacy;
-defined('MOODLE_INTERNAL') || die();
 
 /**
  * Privacy provider class.
